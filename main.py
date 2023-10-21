0c558141acf452d5d76467ecbce87ce63ed02635
Shubh Raheja <shubh42003@gmail.com>
2023-10-21 16:32:56 +0530

    21 Oct

---
+++ main.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import joblib
+background_color = """
+<style>
+body {
+    background-color: #f8f7f2; 
+}
+</style>
+"""
+
+# Adding a local image
+st.image("img.png", caption=" ", use_column_width=True)
+
+
+st.markdown(background_color, unsafe_allow_html=True)
+
+# Load the pre-trained model
+#model = joblib.load('model/model.pkl')
+
+# Create a function to predict water potability
+def predict_potability(input_data):
+    # Ensure input data is in the correct order
+    input_data = input_data[['Turbidity', 'Hardness', 'Sulfate', 'Solids', 'Trihalomethanes', 'Chloramines', 'Conductivity', 'Organic_carbon', 'pH']]
+    input_array = np.array(input_data)
+    prediction = model.predict(input_array)
+    return prediction[0][0]
+
+# Streamlit app
+st.title(" ")
+
+st.write("Enter the values for the following water parameters:")
+
+turbidity = st.number_input("Turbidity")
+hardness = st.number_input("Hardness")
+sulfate = st.number_input("Sulfate")
+solids = st.number_input("Solids")
+trihalomethanes = st.number_input("Trihalomethanes")
+chloramines = st.number_input("Chloramines")
+conductivity = st.number_input("Conductivity")
+organic_carbon = st.number_input("Organic Carbon")
+pH = st.number_input("pH")
+
+input_data = pd.DataFrame({
+    'Turbidity': [turbidity],
+    'Hardness': [hardness],
+    'Sulfate': [sulfate],
+    'Solids': [solids],
+    'Trihalomethanes': [trihalomethanes],
+    'Chloramines': [chloramines],
+    'Conductivity': [conductivity],
+    'Organic_carbon': [organic_carbon],
+    'pH': [pH]
+})
+
+if st.button("Predict Potability"):
+    prediction = predict_potability(input_data)
+    if prediction > 0.5:
+        st.write("The water is potable.")
+    else:
+        st.write("The water is not potable.")
+
